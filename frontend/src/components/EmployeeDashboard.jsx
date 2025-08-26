ca8c533aeee1409f577bfdb4e199a98a01ad890c
haider-zaidi <haiderzaidi45h@gmail.com>
2025-08-26 23:15:50 +0530

    final commit

--- frontend/src/components/EmployeeDashboard.jsx
+++ frontend/src/components/EmployeeDashboard.jsx
@@ -4,7 +4,7 @@ import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 
 // const API_BASE = import.meta.env?.VITE_API_BASE || "http://localhost:5000/api";
-const API_BASE = import.meta.env.VITE_API_BASE;
+const API_BASE = "http://localhost:5000/api";
 
 // Cache configuration
 const CACHE_DURATION = 13.5 * 60 * 60 * 1000; // 13.5 hours
